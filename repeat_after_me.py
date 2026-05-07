029499c06703c300faf0336c563365a5fc9a703e
LenaGolovach11 <kemeev.maks@gmail.com>
2026-05-07 18:28:10 +0300

    first commit

---
+++ repeat_after_me.py
@@ -0,0 +1,12 @@
+print("Эта программа повторяет все что ты ей напишешь")
+print()
+print("Введите слово - выход, чтобы остановить программу")
+print()
+
+while True:
+    print()
+    user_input = input()
+    if user_input == "выход":
+        print("Остановка программы...")
+        break
+    print("Вы ввели -", user_input)
